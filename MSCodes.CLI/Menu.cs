a25adb6ebb54241fa9d6b48e944fd84d2f2e6ef3
Maxone52 <Maxwell-52@mail.ru>
2022-02-25 15:50:02 +0500

    добавление классов

--- MSCodes.CLI/Menu.cs
+++ MSCodes.CLI/Menu.cs
@@ -1,4 +1,23 @@
-﻿public class Menu
+﻿using System.Net;
+
+public class Menu
 {
-    
+    private IOperation[] _operations;
+
+    public Menu(IOperation[] operation)
+    {
+        _operations = operation;
+    }
+
+    public string[] GetOperationNames()
+    {
+        List<string> operationNames = new List<string>();
+
+        foreach (IOperation operation in _operations)
+        {
+            operationNames.Add(operation.Name)
+        }
+
+        return operationNames.ToArray();
+    }
 }
